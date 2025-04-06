1b9ea406358e531ea06e5513ab422deb13ae01de
perkele4040 <emil_kobylecki@onet.eu>
2025-04-06 20:40:58 +0200

    fixes to bubble sort parallel v2

--- src/bubble-sort/bubble-sort-parallel-v2.c
+++ src/bubble-sort/bubble-sort-parallel-v2.c
@@ -28,11 +28,12 @@ void parallelBubbleSort() {
 }
 
 int main(){
+    const int* size = SIZE;
     if(MYTHREAD==0) {
         printf("Using %d threads\n", THREADS); 
         const char* filenameInput = "bubble-sort-test-input.txt";
         const char* filenameOutput = "bubble-sort-test-output.txt";
-        const int* size = SIZE;
+        
         int* arr = loadArrayFromFile(filenameInput, &size);
         printf("Starting array: \n");
         for (int i = 0; i < SIZE; i++)
@@ -42,7 +43,7 @@ int main(){
         printf("Outcome array: \n");
         for (int i = 0; i < SIZE; i++)
             printf("%d, ", arr[i]);
-        printf("did bubble sort work?  = %d\n", validateSortingOutput(arr, &size));
+        //printf("did bubble sort work?  = %d\n", validateSortingOutput(arr, &size));
         writeArrayToOutputFile(filenameOutput, &size, arr); }
     return 0;
 }

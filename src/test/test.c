73d83295e93fd87d5f00aaad387ebfcfd544cf40
perkele4040 <emil_kobylecki@onet.eu>
2025-08-25 17:56:12 +0200

    updated gitignore and test

--- src/test/test.c
+++ src/test/test.c
@@ -1,7 +1,7 @@
 /* 
 image dim - relaxed vs strict DONE
-ga optimisation - broadcast NOSYNC vs ALLSYNC DONE
-bubble sort - scatter? broadcast exchange?
-quick sort - scatter
 knapsack - relaxed vs strict DONE
+ga optimisation - BROADCAST NOSYNC vs ALLSYNC DONE
+bubble sort - SCATTER-GATHER NOSYNC VS ALLSYNC - todo
+quick sort - BROADCAST-EXCHANGE VS SCATTER-GATHER
  */
